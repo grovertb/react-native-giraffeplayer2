0deeda0d2a02a9187bbe51a56c19c1efae7a54ba
GroverTB <grover.jtb@gmail.com>
2019-10-21 12:17:36 -0500

    [ UPDATE ] proptypes

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,11 @@
-declare const _default: {
-  play: (url : string) => any;
-  setTitle?: string;
-  setFullScreenOnly?: boolean;
-  setShowTopBar?: boolean;
+type AspectRatio = 'AR_ASPECT_FIT_PARENT' | 'AR_ASPECT_FILL_PARENT' | 'AR_ASPECT_WRAP_CONTENT' | 'AR_MATCH_PARENT' | 'AR_16_9_FIT_PARENT' | 'AR_4_3_FIT_PARENT';
+
+declare const GPlayer: {
+  play: (url: string) => void;
+  setTitle?: (title: string) => void;
+  setFullScreenOnly?: (isFullScreenOnly :boolean) => void;
+  setShowTopBar?: (isShowTopbar: boolean) => void;
+  setAspectRatio?: (aspectRatio: AspectRatio) => void;
 }
 
-export default _default;
+export default GPlayer;
